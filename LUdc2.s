e7bc33cf130c3c166cd7f098b7773bceb2252dd3
D.L.Buunk <dlbuunk@gmail.com>
2011-05-13 15:55:57 +0200

    beta loop done

--- LUdc2.s
+++ LUdc2.s
@@ -60,7 +60,8 @@ setup_a:
 	movq	%rdx,%rcx	# dimensions
 	movq	%rsi,%r9	# ** matrix bT
 	movq	%rdx,%rax	# dimensions
-	shlq	$2,%rax		# * matrix bT
+	shlq	$2,%rax
+	addq	%rsi,%rax	# * matrix bT
 	xorq	%rbx,%rbx	# size of a row of bT
 setup_b:
 	addq	$0x10,%rbx
@@ -110,17 +111,85 @@ beta_loop:
 	addq	%r9,%rcx
 	movq	(%rcx),%rsi
 
+	# clear xmm0
+	xorps	%xmm0,%xmm0
+
 	# inside the beta loop, there is an inner loop for the dot-product
+	movq	%rbx,%rcx
+inner_loop_a:
+	cmpq	$4,%rcx
+	jb	inner_a_almost
+
+	# multiply and add 4 floats at the same time
+	movaps	(%rdi),%xmm1
+	movaps	(%rsi),%xmm2
+	mulps	%xmm2,%xmm1
+	addq	$0x10,%rdi
+	addq	$0x10,%rsi
+	addps	%xmm1,%xmm0
+
+	subq	$4,%rcx
+	jmp	inner_loop_a
+
+inner_a_almost:
+	jrcxz	inner_a_0
+
+	# 3/2/1 left
+	xorps	%xmm1,%xmm1
+	xorps	%xmm2,%xmm2
+	movss	(%rdi),%xmm1
+	movss	(%rsi),%xmm2
+	addq	$0x4,%rdi
+	addq	$0x4,%rsi
+	decq	%rcx
+	jrcxz	inner_a_end
+
+	# 2/1 left
+	pslldq	$4,%xmm1
+	pslldq	$4,%xmm2
+	movss	(%rdi),%xmm1
+	movss	(%rsi),%xmm2
+	addq	$0x04,%rdi
+	addq	$0x04,%rsi
+	decq	%rcx
+	jrcxz	inner_a_end
+
+	# 1 left
+	pslldq	$4,%xmm1
+	pslldq	$4,%xmm2
+	movss	(%rdi),%xmm1
+	movss	(%rsi),%xmm2
+	addq	$0x04,%rdi
+	addq	$0x04,%rsi
+
+inner_a_end:
+	mulps	%xmm2,%xmm1
+	addps	%xmm1,%xmm0
+
+inner_a_0:
+	# now we only have to add up the 4 values to 1
+	movaps	%xmm0,%xmm1
+	psrldq	$8,%xmm1
+	addps	%xmm1,%xmm0
+	movaps	%xmm0,%xmm1
+	psrldq	$4,%xmm1
+	addss	%xmm1,%xmm0
+
+	# now, get the "a" value and substract.
+	# calculate the pointer first, we can use %rcx
+	movq	%rbx,%rcx
+	shlq	$3,%rcx
+	addq	%r8,%rcx
+	movq	(%rcx),%rdi
 	movq	%rax,%rcx
-inner_loop:
-	
-
-
-
-
-
-
-
+	shlq	$2,%rcx
+	addq	%rcx,%rdi
+	movss	(%rdi),%xmm1
+	subss	%xmm0,%xmm1
+	# store in "a" matrix
+	movss	%xmm1,(%rdi)
+	# store in "bT" matrix
+	movss	%xmm1,(%rsi)
 
 	# end of beta loop
 	incq	%rbx
@@ -144,7 +213,7 @@ alpha_loop:
 
 	# return to calling function
 	movq	%r8,%rax
-
+end:
 	addq	$0x10,%rsp
 	popq	%rbp
 	popq	%rdi
